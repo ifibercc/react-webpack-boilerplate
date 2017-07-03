27f83527213f76e25d7065bbf38a5fc30d407352
jingchu.fhb <jingchu.fhb@alibaba-inc.com>
2017-07-03 11:32:11 +0800

    first blood

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    main: './src/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(scss|css)$/,
+        loader: 'style-loader!css-loader!sass-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+        loader: 'file-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+        loader: 'file-loader',
+        query: {
+          name: '[name].[ext]?[hash]',
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    port: 8888,
+    proxy: {
+      '/api': 'http://122.114.38.213',
+      '/mock': 'http://localhost:8888',
+    },
+  },
+  devtool: '#eval-source-map',
+  plugins: [new webpack.optimize.UglifyJsPlugin()],
+};
